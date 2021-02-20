cd3260e460530acd57a337c779370e8223d6a592
haytastan <haytastan@gmail.com>
2021-02-20 22:04:40 +0300

    initial commit (xcode 10.1 - MacOS 10.13 High Sierra -MacBook Air 4,1 - by Dr. Hayati TAŞTAN, Feb 20, 2021, Ankara)

---
+++ Tabbed_app_HT/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Tabbed_app_HT
+//
+//  Created by Hayati TASTAN on 20.02.2021.
+//  Copyright © 2021 Hayati TASTAN. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
